988a6cea7f787db45e3a2aeac457301c292e6967
Cesar Menjivar <cez_r@hotmail.com>
2022-03-08 13:18:10 -0500

    Added check_duplicate function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cezmenji <cezmenji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 12:55:41 by cemenjiv          #+#    #+#             */
-/*   Updated: 2022/03/07 18:00:16 by cezmenji         ###   ########.fr       */
+/*   Updated: 2022/03/08 13:14:53 by cezmenji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@ void    error(void)
 {
     ft_putstr_fd("Error\n", STDERR_FILENO);
     exit (EXIT_FAILURE);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
 
 int	ft_atol(const char *str)
@@ -57,6 +71,23 @@ void    check_if_int(int argc, char **argv)
     }
 }
 
+void    check_duplicate(char **argv , int i)
+{
+    int j;
+
+    while (argv[i] != 0)
+    {
+        j = i + 1;
+        while (argv[j])
+        {
+            if (ft_strcmp(argv[i], argv[j]) == 0)
+                    error();
+            j++;
+        }
+        i++;
+    }
+}
+
 int main (int argc, char **argv)
 {
     t_stack   *head;  //head est le premier element de la linked list. 
@@ -65,7 +96,8 @@ int main (int argc, char **argv)
     if (argc > 1)
     {
         argc = (argc - 1);
-        check_if_int (argc, argv);
+        check_if_int (argc, argv); // Valide que les arguments soient des chiffres et aussi respecte les INT_MIN et INT_MAX
+        check_duplicate(argv, 1);// Valide qu'il n'y a pas de doublons parmi les chiffres donnees 
         while (argc > 0)
         {
             push (&head, ft_atol(argv[argc]));
